4876ecc49f6f2de86e9626af3077e31dc8ecf598
Troxill <todorgerdzhikov19a@gmail.com>
2025-06-07 12:51:48 +0300

    Database set up

--- app/Models/User.php
+++ app/Models/User.php
@@ -45,4 +45,19 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function categories()
+    {
+        return $this->hasMany(Category::class);
+    }
+
+    public function transactions()
+    {
+        return $this->hasMany(Transaction::class);
+    }
+
+    public function budgets()
+    {
+        return $this->hasMany(Budget::class);
+    }
 }
